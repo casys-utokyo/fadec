de51ab38796ff615b34b7bc0da1425ab764967c3
hashi0203 <n.hsmt1123@gmail.com>
2022-10-18 00:57:43 +0900

    remove unnecessary part

--- dev/quantizer/keyframe_buffer2.py
+++ dev/quantizer/keyframe_buffer2.py
@@ -32,11 +32,7 @@ class KeyframeBuffer:
                 self.last_pose = pose
                 return 0  # pose is available, new frame added but buffer was empty, this is the first frame, no depth map prediction will be done
             else:
-                # last_pose = self.buffer[-1][0]
-
                 combined_measure, R_measure, t_measure = pose_distance(pose, self.last_pose)
-                # with open('errors.txt', 'a') as f:
-                #     f.writelines('%.3f\n' % combined_measure)
 
                 if combined_measure >= self.keyframe_pose_distance:
                     self.last_pose = pose
@@ -86,45 +82,3 @@ class KeyframeBuffer:
         for index in indices:
             measurement_frames.append(buffer_array[index])
         return measurement_frames
-
-
-class SimpleBuffer:
-    def __init__(self, buffer_size, store_return_indices):
-        self.buffer = deque([], maxlen=buffer_size + 1)
-        self.__tracking_lost_counter = 0
-        self.__store_return_indices = store_return_indices  # mostly required for simulation of the frame selection
-
-    def try_new_keyframe(self, pose, image, index=None):
-        if self.__store_return_indices and index is None:
-            raise ValueError("Storing and returning the frame indices is requested in the constructor, but index=None is passed to the function")
-
-        if is_pose_available(pose):
-            self.__tracking_lost_counter = 0
-            if len(self.buffer) == 0:
-                if self.__store_return_indices:
-                    self.buffer.append((pose, image, index))
-                else:
-                    self.buffer.append((pose, image))
-                return 0  # pose is available, new frame added but buffer was empty, this is the first frame, no depth map prediction will be done
-            else:
-                if self.__store_return_indices:
-                    self.buffer.append((pose, image, index))
-                else:
-                    self.buffer.append((pose, image))
-                return 1  # pose is available, new frame added, everything is perfect, and we will predict a depth map later
-        else:
-            self.__tracking_lost_counter += 1
-
-            if self.__tracking_lost_counter > 30:
-                if len(self.buffer) > 0:
-                    self.buffer.clear()
-                    return 2  # a pose reading has not arrived for over a second, tracking is now lost
-                else:
-                    return 3  # we are still very lost
-            else:
-                return 4  # pose is not available right now, but not enough time has passed to consider lost, there is still hope :)
-
-    def get_measurement_frames(self):
-        measurement_frames = list(self.buffer)[:-1]
-        return measurement_frames
-
