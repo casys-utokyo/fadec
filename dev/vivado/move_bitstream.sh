3db0e0d9402923c6e77affc1cb55208018f844f3
hashi0203 <n.hsmt1123@gmail.com>
2022-12-27 16:35:16 +0900

    update scripts

--- dev/vivado/move_bitstream.sh
+++ dev/vivado/move_bitstream.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 if [ $# -ne 2 ]; then
-    echo "指定された引数は$#個です。" 1>&2
-    echo "実行するには2個の引数が必要です。" 1>&2
+    echo "Number of args should be 2 but $#" 1>&2
     exit 1
 fi
 
-src_dir=$1
-dst_dir=$2
-proj_name=${1##*/}
+src_dir=${1%/}
+dst_dir=${2%/}
 
-scp -3 ${src_dir}/${proj_name}.runs/impl_1/design_1_wrapper.bit ${dst_dir}/design_1.bit || echo ".bit not found"
-scp -3 ${src_dir}/${proj_name}.gen/sources_1/bd/design_1/hw_handoff/design_1.hwh ${dst_dir}/design_1.hwh || echo ".hwh not found"
+echo "scp -3 ${src_dir}/design_1/design_1.bit ${dst_dir}/design_1.bit"
+scp -3 ${src_dir}/design_1/design_1.bit ${dst_dir}/design_1.bit || echo ".bit src or dst not found" 1>&2
+echo "scp -3 ${src_dir}/design_1/design_1.hwh ${dst_dir}/design_1.hwh"
+scp -3 ${src_dir}/design_1/design_1.hwh ${dst_dir}/design_1.hwh || echo ".hwh src or dst not found" 1>&2
